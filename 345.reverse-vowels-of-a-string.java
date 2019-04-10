a4404bd7b097bc10c1e5e79f56f4878a07f5cf18
Max Xu <max.xu@biqmind.com>
2019-04-11 00:38:52 +0800

    4 easy string problems from Microsoft today I am too busy to solve the medium or hard problems

--- 345.reverse-vowels-of-a-string.java
+++ 345.reverse-vowels-of-a-string.java
@@ -13,31 +13,43 @@
  *
  * Write a function that takes a string as input and reverse only the vowels of
  * a string.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: "hello"
  * Output: "holle"
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: "leetcode"
  * Output: "leotcede"
- * 
- * 
+ *
+ *
  * Note:
  * The vowels does not include the letter "y".
- * 
- * 
- * 
+ *
+ *
+ *
  */
 class Solution {
     public String reverseVowels(String s) {
-        
+        String vowels = "aeiouAEIOU";
+        char[] sc = s.toCharArray();
+        int start = 0, end = s.length() - 1;
+        while (start < end) {
+            while (start < end && !vowels.contains(sc[start] + "")) start++;
+            while (start < end && !vowels.contains(sc[end] + "")) end--;
+            char temp = sc[start];
+            sc[start] = sc[end];
+            sc[end] = temp;
+            start++;
+            end--;
+        }
+        return new String(sc);
     }
 }
 
---
+++ 67.add-binary.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=67 lang=java
+ *
+ * [67] Add Binary
+ *
+ * https://leetcode.com/problems/add-binary/description/
+ *
+ * algorithms
+ * Easy (38.32%)
+ * Total Accepted:    289.2K
+ * Total Submissions: 750.9K
+ * Testcase Example:  '"11"\n"1"'
+ *
+ * Given two binary strings, return their sum (also a binary string).
+ *
+ * The input strings are both non-empty and contains only characters 1 or 0.
+ *
+ * Example 1:
+ *
+ *
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ *
+ * Example 2:
+ *
+ *
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
+ *
+ */
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
+
